c84386f307e44fe43264db51b00c4ef4a398ebc0
Olivier Briand <olivier.briand@nxp.com>
2020-03-26 10:18:25 +0100

    Set Eeprom to V99

--- Marlin/src/module/configuration_store.cpp
+++ Marlin/src/module/configuration_store.cpp
@@ -37,7 +37,7 @@
  */
 
 // Change EEPROM version if the structure changes
-#define EEPROM_VERSION "V69"
+#define EEPROM_VERSION "V99"
 #define EEPROM_OFFSET 100
 
 // Check the integrity of data offsets.
