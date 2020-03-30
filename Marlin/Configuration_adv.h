efde6ca2a6abb3e05f3f909bc1d65dbbe0a2ece8
Olivier Briand <olivier.briand@nxp.com>
2020-03-30 22:34:11 +0200

    Fixed I,J and K moove using DOGM menu

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -863,7 +863,16 @@
 // @section lcd
 
 #if EITHER(ULTIPANEL, EXTENSIBLE_UI)
+#if NON_E_AXES == 6
+  #define MANUAL_FEEDRATE { 50*60, 50*60, 4*60, 50*60, 50*60, 50*60, 60 } // Feedrates for manual moves along X, Y, Z, E, I, J, K from panel
+#elif NON_E_AXES == 5
+  #define MANUAL_FEEDRATE { 50*60, 50*60, 4*60, 50*60, 50*60, 60 } // Feedrates for manual moves along X, Y, Z, E, I, J from panel
+#elif NON_E_AXES == 4
+  #define MANUAL_FEEDRATE { 50*60, 50*60, 4*60, 50*60, 60 } // Feedrates for manual moves along X, Y, Z, E, I from panel
+#else // default for NUM_E_AXES == 3
   #define MANUAL_FEEDRATE { 50*60, 50*60, 4*60, 60 } // Feedrates for manual moves along X, Y, Z, E from panel
+#endif
+
   #define SHORT_MANUAL_Z_MOVE 0.025 // (mm) Smallest manual Z move (< 0.1mm)
   #if ENABLED(ULTIPANEL)
     #define MANUAL_E_MOVES_RELATIVE // Display extruder move distance rather than "position"
